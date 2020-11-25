3c58ee7eb0a8b206829b3fa61357392e36b29e9a
Ronaldo Rugieri <71158407+rugieri@users.noreply.github.com>
2020-11-25 13:01:48 -0300

    Add files via upload
    A program that counts words from a text

---
+++ wordcounter.js
@@ -0,0 +1,9 @@
+// A simple program that counts words from a text
+//just inserting a text on the prompt
+function numberWords(word) {
+	for (let i = 0; i < word.split(); i++)
+		word [i]++
+	return word.split(" ").length
+}
+var words = prompt("insert a text here to count the words. ");
+console.log(numberWords(words));
