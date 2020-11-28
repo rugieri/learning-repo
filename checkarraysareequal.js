92f3e63e380674df2b8172a5a3e082f73eff3d17
Ronaldo Rugieri <71158407+rugieri@users.noreply.github.com>
2020-11-28 09:58:02 -0300

    Update checkarraysareequal.js

--- checkarraysareequal.js
+++ checkarraysareequal.js
@@ -1,9 +1,6 @@
 //a function that returns true if two arrays contain identical values, and false otherwise.
 function checkEquals(arr1, arr2) {
-	let arr1str = JSON.stringify(arr1);
-	let arr2str = JSON.stringify(arr2);
-	
-	if (arr1str === arr2str) {
+	if (arr1.toString() === arr2.toString()) {
   	return true 
   } else {
   	return false
