36897fdb7aeadff2611d0a01262c29af16078d3b
Ronaldo Rugieri <71158407+rugieri@users.noreply.github.com>
2021-04-23 23:05:03 -0300

    Add files via upload

---
+++ try_catch_example.js
@@ -0,0 +1,19 @@
+const getMonthName = (mo) =>{
+    mo = mo - 1; //Adjust month number for arrya index(1 = JAN, 12 - DEC)
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
+                    'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
+    if (months[mo]) {
+        return months[mo];
+    } else {
+        throw 'InvalidMonthNo'; //throw keyword is used here
+    }                 
+}
+
+try {// statements to try
+    getMonthName = getMonthName(myMonth);//function could throw exception
+}
+
+catch (e) {
+    getMonthName = 'unknown';
+    logMyErrors(e); //pass exception object to erro handler(i.e your function)
+}
